994d535b3f83099fab9aa4f3f1e2e146d5cdfa27
bulatkhu <hudainazarov.b@gmail.com>
2023-04-23 13:37:22 +0200

    feat: Implement player switching using number keys

---
+++ Assets/Scripts/PlayerSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerSwitcher : MonoBehaviour
+{
+    public GameObject[] players;
+    private int activePlayer;
+    
+    void Update()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (Input.GetKeyDown((i + 1).ToString()))
+            {
+                SwitchPlayer(i);
+            }
+        }
+    }
+
+    void SwitchPlayer(int newActivePlayer)
+    {
+        if (newActivePlayer != activePlayer && newActivePlayer < players.Length)
+        {
+            PlayerController currentController = players[activePlayer].GetComponent<PlayerController>();
+            PlayerController newController = players[newActivePlayer].GetComponent<PlayerController>();
+
+            if (currentController != null && newController != null)
+            {
+                currentController.enabled = false;
+                newController.enabled = true;
+            }
+
+            players[activePlayer].SetActive(false);
+            players[newActivePlayer].SetActive(true);
+            activePlayer = newActivePlayer;
+        }
+    }
+}
