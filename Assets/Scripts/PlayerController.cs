6ad08f05edfee758d8a146becea78dc75710d4f4
Jazin <70114019+jazin7@users.noreply.github.com>
2023-04-24 23:41:27 +0200

    flips

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,10 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5.00f;
-    private float[] speedMultipliers = { 1.0f, 2.0f, 4.0f };
-    private int currentSpeedIndex = 0;
-    
+    [SerializeField] float speed = 3f;
+    SpriteRenderer sr;
+    void Awake() => sr = GetComponent<SpriteRenderer>();
+
     void Update()
     {
         Move();
@@ -14,24 +14,34 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        
-        Vector3 rightMovement = new Vector3(moveHorizontal, 0f, 0f) * moveSpeed * Time.deltaTime;
-        Vector3 upMovement = new Vector3(0f, moveVertical, 0f) * moveSpeed * Time.deltaTime;
+        Vector3 moveVector = Vector3.zero;
 
-        // Check if the Shift key is held down
+        // Get input and save state in moveVector
+        if (Input.GetKey(KeyCode.W)) moveVector.y = 1;
+        if (Input.GetKey(KeyCode.A)) moveVector.x = -1;
+        if (Input.GetKey(KeyCode.S)) moveVector.y = -1;
+        if (Input.GetKey(KeyCode.D)) moveVector.x = 1;
+
+        // change speed
         if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
         {
-            currentSpeedIndex = (currentSpeedIndex + 1) % speedMultipliers.Length;
+            if (speed == 3f) speed = 6f;
+            else if (speed == 6f) speed = 12f;
+            else if (speed == 12f) speed = 3f;
         }
-        
-        float speedMultiplier = speedMultipliers[currentSpeedIndex];
-        
-        rightMovement *= speedMultiplier;
-        upMovement *= speedMultiplier;
 
-        transform.position += rightMovement + upMovement;
+        // Normalize vector, so that magnitude for diagonal movement is also 1
+        moveVector.Normalize();
+
+        // Frame rate independent movement
+        transform.position += Time.deltaTime * speed * moveVector;
+
+        Vector3 characterScale = transform.localScale;
+        if (moveVector.x < 0)
+            characterScale.x = -1;
+        else if (moveVector.x > 0)
+            characterScale.x = 1;
+        transform.localScale = characterScale;
     }
 
     void HandleColorChange()
